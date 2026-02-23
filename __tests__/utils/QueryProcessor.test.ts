e07ace27db616338e01687fed573e49fd2974434
Yari Gonzalez <yaritzag@andrew.cmu.edu>
2026-02-23 16:31:10 +0000

    adding andrewID test

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -24,5 +24,13 @@ describe("QueryProcessor", () => {
         expect(response).toBe((
             "Rohan"
           ));
-    })
+    });
+
+    test('should return andrew id', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "yaritzag"
+        ));
+    });
 });
