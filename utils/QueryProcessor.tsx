a60df614eacabf310a2d8cf558e5fa973d16f216
Yari Gonzalez <yaritzag@andrew.cmu.edu>
2026-02-24 14:50:46 +0000

    what is your name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "yaritzag";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
